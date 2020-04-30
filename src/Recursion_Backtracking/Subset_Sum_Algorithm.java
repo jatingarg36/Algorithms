afc694c727193060bfbf2459fafab9e9e59e6238
jatin-773762 <jatin.manugarg@gmail.com>
2020-04-30 17:02:39 +0530

    Initial Commit

--- src/Recursion_Backtracking/Subset_Sum_Algorithm.java
+++ src/Recursion_Backtracking/Subset_Sum_Algorithm.java
@@ -1,18 +1,57 @@
 package Recursion_Backtracking;
 
+
+/**
+ * here is an backtracking algorithm to find a subset from a given set whose sum add up to a given number.
+ */
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Subset_Sum_Algorithm {
-    private static int n;
-    private static ArrayList<Integer> set = new ArrayList<>();
+    private static int k;
+    private static ArrayList<Integer> set ,subset;
 
     public static void main(String[] args) {
+        set = new ArrayList<>();
+        subset = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-        n = input.nextInt();
-        for(int i=0;i<n;i++){
+        int n = input.nextInt();
+        for(int i = 0; i< n; i++){
             set.add(input.nextInt());
         }
-        System.out.println(set.get(1));
+        k = input.nextInt();
+
+        findSubset(0,0);
+
+        System.out.println("Want to try another no. y/n: ");
+        String check = input.next();
+        if (check.equals("y") || check.equals("Y")) {
+            main(null);
+        } else {
+            System.out.println("Thank You :) ");
+            System.exit(0);
+        }
+    }
+
+    private static boolean findSubset(int x,int sum) {
+        if(sum==k){
+            for(Integer integer : subset){
+                System.out.print(integer+" ");
+            }
+            return true;
+        }
+        for(int i=x;i<set.size();i++){
+            int p =set.get(i);
+            if(sum<k){
+                subset.add(p);
+                sum+=p;
+                if(findSubset(i+1,sum))
+                    return true;
+                sum-=p;
+                subset.remove(subset.size()-1);
+            }
+        }
+        return false;
     }
 }
